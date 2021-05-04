9237a986a8bafc9c9cb118011c092213962bb261
Nikhil <nikhilpandey1215@gmail.com>
2021-05-04 18:06:29 +0530

    added posts route and controller

---
+++ controller/posts_controller.js
@@ -0,0 +1,3 @@
+module.exports.viewPosts = function (req,res){
+    res.end('<h1>This is users posts...</h1>')
+}
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const user_controller = require('../controller/user_controller');
+const posts_controller = require('../controller/posts_controller');
 
 router.get('/profile',user_controller.profile);
+router.get('/posts',posts_controller.viewPosts);
 
 module.exports = router;
